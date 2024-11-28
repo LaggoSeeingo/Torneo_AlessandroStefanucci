cc1a9bc9a436371c586a647770529b7a9bba1212
AlessandroStefanucci <aless.stefanucci@gmail.com>
2024-11-28 09:20:49 +0100

    Creata struttura generale della classe squadra

--- src/torneo_stefanucci/Squadra.java
+++ src/torneo_stefanucci/Squadra.java
@@ -8,6 +8,35 @@ package torneo_stefanucci;
  *
  * @author B045_14
  */
-public class Squadra {
+
+import java.util.Random;
+
+public class Squadra implements Runnable{
     
+    private int idSquadra;
+    private int punti;
+    private Random random; //Generatore di numeri casuali, verrà usato in futuro nello sviluppo
+    private Arbitro arbitro;
+    
+    public Squadra(int idSquadra, Arbitro arbitro){
+        this.idSquadra = idSquadra;
+        punti = 0;
+        this.arbitro = arbitro;
+        random = new Random(); //Inizializzazione generatore numeri casuali
+    }
+    
+    public int getIdSquadra(){
+        return idSquadra;
+    }
+    
+    public int getPunti(){
+        return punti;
+    }
+    
+    
+    @Override
+    public void run(){
+        //codice
+    }
 }
+
