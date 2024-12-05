a93811ac35aeb46cc17bf3f76942c8e10b9037dd
AlessandroStefanucci <aless.stefanucci@gmail.com>
2024-12-05 09:11:12 +0100

    aggiunte classi di lettura e scrittura, codice preso da https://github.com/mciuchetti/GestioneFile/tree/master/src/gestionefile

--- src/torneo_stefanucci/Write.java
+++ src/torneo_stefanucci/Write.java
@@ -4,10 +4,58 @@
  */
 package torneo_stefanucci;
 
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author B045_14
+ * @author MC
+ * @version 12/01/23
  */
-public class Write {
+
+public class Write implements Runnable{
+
+    String nomeFile;
     
+    public Write(String nomeFile){
+        this.nomeFile = nomeFile;
+    }
+    
+    @Override
+    public void run() {
+        scrivi();
+    }
+    /**
+     * Scrive un file di testo usando la classe BufferedWriter
+     */
+    public void scrivi(){
+        BufferedWriter br=null;
+        
+        try {
+            //1) apro il file
+            br = new BufferedWriter(
+                    new FileWriter(nomeFile));
+            //2) scrivo nel buffer
+            br.write("File in output");
+            br.write("\n\r");
+            //3) svuoto il buffer e salvo nel file i dati
+            br.flush();         
+        } catch (IOException ex) {
+            Logger.getLogger(Write.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            if (br!=null)
+                try {
+                    //4)chiudo lo stream in uscita
+                    br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Write.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+        }
+    }
 }
